0f7c5bd0418d123ce281f0d8a5930606f14609a0
BrendonsDad <orraubury@gmail.com>
2022-04-02 23:16:15 -0600

    Was not able to update with git but, made new

---
+++ code/settings.py
@@ -0,0 +1,18 @@
+# level_map = [
+# '                            ',
+# '                            ',
+# '                            ',
+# ' XX    XX X             XX   ',
+# ' XX P                       ',
+# ' XXXX         XX         XX ',
+# ' XXXX       XX              ',
+# ' XX    X  XXXX    XX  XX    ',
+# '       X  XXXX    XX  XX X   ',
+# '    XXXX  XXXXXX  XX  XXXX  ',
+# 'XXXXXXXX  XXXXXX  XX  XXXX  '
+# ]
+
+vertical_tile_number = 11
+tile_size = 64
+screen_width = 1200
+screen_height = vertical_tile_number * tile_size
